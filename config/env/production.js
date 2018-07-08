1eab4f26523b67b7d418f9b027d4fdf26a13d633
razumforce <razum.force@gmail.com>
2018-07-08 14:07:17 +0300

    change baseUrl in prod

--- config/env/production.js
+++ config/env/production.js
@@ -371,7 +371,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
+    baseUrl: 'https://sailsrf.herokuapp.com',
     internalEmailAddress: 'support@example.com',
 
     // mailgunDomain: 'mg.example.com',
